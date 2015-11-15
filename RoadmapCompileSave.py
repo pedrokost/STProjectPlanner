51cf0c3d1ea44f63fd3109ecb23fe06a085e71aa
Pedro Kostelec <pedrokost@gmail.com>
2015-11-15 22:02:15 +0100

    Run Trello sync before compiling

--- RoadmapCompileSave.py
+++ RoadmapCompileSave.py
@@ -5,8 +5,8 @@ class RoadmapCompileSave(sublime_plugin.EventListener):
 		file_name = view.file_name()
 		if file_name.endswith('.roadmap.md'):
 			print('RoadmapCompileSave: compiling roadmap')
-			view.run_command('roadmap_compile')
 			view.run_command('roadmap_trello')
+			view.run_command('roadmap_compile')
 			# import profile
 			# profile.runctx("view.run_command('roadmap_compile')", {}, {'view': view}, filename="/home/pedro/roadmapcompileplugin.profile")
 
