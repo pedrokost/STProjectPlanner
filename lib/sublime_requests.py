98d6c7d15f0c4987c895b0f5b6c8a2bf4fbeab33
Pedro Kostelec <pedrokost@gmail.com>
2015-12-02 22:50:18 +0100

    Use the requests Package Control dependency instead of bundling my own

--- lib/sublime_requests.py
+++ lib/sublime_requests.py
@@ -2,8 +2,8 @@
 # Copyright (c) 2011 Brad Greenlee
 
 import re
-from . import requests
-from .requests.status_codes import codes
+import requests
+from requests.status_codes import codes
 try:
     import http.client as httplib
 except ImportError:
